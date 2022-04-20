65a59abe2bc232c41a9a8d91f506d59ea9a201d7
aratius <arata1129matsu@icloud.com>
2022-04-20 16:31:40 +0900

    tsx側実装準備

---
+++ src/components/editor/modules/pager.tsx
@@ -0,0 +1,12 @@
+import { PureComponent, ReactElement } from "react";
+
+/**
+ * ページャー
+ */
+export default class Pager extends PureComponent {
+
+	public render(): ReactElement {
+		return <></>
+	}
+
+}
---
+++ src/components/editor/modules/toolBar.tsx
@@ -0,0 +1,9 @@
+import { PureComponent, ReactElement } from "react";
+
+export default class ToolBar extends PureComponent {
+
+	public render(): ReactElement {
+		return <></>
+	}
+
+}
