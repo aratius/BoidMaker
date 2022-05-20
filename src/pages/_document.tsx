254bce6b0741a999a43a5915ad0d9f5d1eaa398f
aratius <arata1129matsu@icloud.com>
2022-05-20 23:19:31 +0900

    pwa実装

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,11 @@ export default class Document extends NextDocument {
 	public render(): ReactElement {
 		return (
 			<Html lang="ja" >
-				<Head />
+				<Head>
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="apple-touch-icon" href="icon.png" />
+					<meta name="theme-color" content="#fff" />
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
